efefa801e5d90be6865cb6def1b42dfb089bfb17
Riccardo Govoni <battlehorse@gmail.com>
2009-12-09 21:59:59 +0000

    Extra accessor for number of snippet views +1 (to be used on the view page to include the current visualization)

--- snipbin/vo.py
+++ snipbin/vo.py
@@ -4,6 +4,7 @@ class SnipPageVO(object):
   
   def __init__(self, snippage):
     self.m = snippage
+    self.views_including_this = snippage.views + 1
     self.key = snippage.key()
     self.snippets_count = snippage.snippets.count()
     self.abuses_count = snippage.abuses.count()
