dc1ff4970eaf36948ea7cff9ce8f32e962912094
Riccardo Govoni <battlehorse@gmail.com>
2009-12-16 09:21:01 +0000

    Refactored user capabilities in the test page too

--- snipbin/test/test.py
+++ snipbin/test/test.py
@@ -12,20 +12,15 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+import snipglobals
+
 
 class TestHandler(webapp.RequestHandler):
 
   def get(self):
-    user = users.get_current_user()
-    xsrf_token = hashlib.md5('%s-%s' % (user.user_id(), random.random())).hexdigest()
-    
-    template_values = {
-      'xsrf_token': xsrf_token,
-      'logout_url': users.create_logout_url('/'),
-    }
-    
+    user, template_values = snipglobals.get_user_capabilities(self.request,
+                                                              self.response)        
     path = os.path.join(os.path.dirname(__file__), '../templates/test.html')
-    self.response.headers.add_header('Set-Cookie', 'xsrf_token=%s' % xsrf_token)
     self.response.out.write(template.render(path, template_values))    
 
 
