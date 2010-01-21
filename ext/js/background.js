b5bfc3c3cc487e2f25da2f88a09a99103915da4e
Riccardo Govoni <battlehorse@gmail.com>
2010-01-21 10:12:13 +0000

    Documentation and refactoring of extension code.

--- ext/js/background.js
+++ ext/js/background.js
@@ -1,25 +1,149 @@
-var snippage = {
- title: '',
- snippets: []
-};
+// Copyright (c) 2009 Riccardo Govoni. All rights reserved.
+// Use of this source code is governed by the MIT License and Creative Commons
+// Attribution License 3.0. Both of them can be found in the LICENSE file.
 
-var stored_snippage = localStorage.getItem('snippage');
-if (stored_snippage) {
-  snippage = $.evalJSON(stored_snippage);
-  updateBadgeText();
-}
+/*
+  A snippage is the container for all the snippets the user has collected so
+  far, and it's synced to localStorage.
+*/
+var snippage;
 
+/*
+  Id of the 'dump' tab, showing the list of collected snippets, or null if
+  not open.
+*/
 var dump_tab_id;
+
+/*
+  Unique id and url of the currently selected tab.
+*/
 var cur_tab_id;
 var cur_tab_url;
-chrome.tabs.getSelected(null, function(tab) {
-  cur_tab_url = tab.url;
-  cur_tab_id = tab.id;
-});
 
+/*
+  Mapping of tab ids to snippy activation statuses.
+  Keeps track of which tabs are currently showing the snippy capture UI.
+  
+  It may contain some stale ids, since we don't cleanup this structure
+  when the user closes tabs and opens new ones.
+*/
 var activation = {};
+
+/*
+  Mapping of tab ids to readiness statuses (strings).
+  We don't want to let the user activate snippy while webpages have not
+  finished loading yet (otherwise snippy.js might not be yet available), hence
+  we keep track of their readiness status.
+
+  It may contain some stale ids, since we don't cleanup this structure
+  when the user closes tabs and opens new ones.
+*/
 var readiness = {};
 
+/* Gentlemen, start your engines ... */
+init();
+
+
+/*
+  Load a snippage saved in a previous session from localStorage. If not found, 
+  create an empty one. Then:
+  - Update the badget text,
+  - figure out the tab we're currently on,
+  - hook up Chrome event listeners.
+*/
+function init() {
+  snippage = {
+   title: '',
+   snippets: []
+  };
+  
+  var stored_page = localStorage.getItem('snippage');
+  if (stored_page) {
+    snippage = $.evalJSON(stored_page);
+  }
+  updateBadgeText();
+  chrome.tabs.getSelected(null, function(tab) {
+    cur_tab_url = tab.url;
+    cur_tab_id = tab.id;
+  });
+  
+  addEventListeners();
+}
+
+/*
+  Register listeners for all the Chrome events we need.
+*/
+function addEventListeners() {
+  
+  // When the user closes a tab, verify if he's closing the 'dump' tab. If
+  // so, null our reference.
+  chrome.tabs.onRemoved.addListener(function(tab_id) {
+    if (dump_tab_id == tab_id) {
+      dump_tab_id = null;
+    }
+  });
+
+  // If the user selects the 'dump' tab, send it a reload message (the user
+  // might have added some snippets since the last time we rendered it).
+  //
+  // Also update the cur_tab_id and cur_tab_url variables.
+  chrome.tabs.onSelectionChanged.addListener(function(tab_id) {
+    cur_tab_id = tab_id;
+    if (dump_tab_id == tab_id) {
+      chrome.tabs.sendRequest(tab_id, {'reload': true}, function(response){});
+    }
+    chrome.tabs.get(tab_id, function(tab) {
+      cur_tab_url = tab.url;
+    });
+  });
+
+  // Keep track of the readiness status of tabs.
+  chrome.tabs.onUpdated.addListener(function(tab_id, change_info) {
+    if (change_info.status == "loading") {
+      console.log("Readiness of " + tab_id + " is 'loading'");
+      readiness[tab_id] = "loading";
+    } else if (change_info.status == "complete") {
+      console.log("Readiness of " + tab_id + " is 'complete'");
+      readiness[tab_id] = "ready";
+    }
+    
+    // TODO(battlehorse): is this really needed? I can't remember...
+    chrome.tabs.get(tab_id, function(tab) {
+      cur_tab_url = tab.url;
+    });
+  });
+
+  // Handle messages coming from the injected Snippy content script.
+  chrome.extension.onRequest.addListener(
+    function(request, sender, sendResponse) {
+      if (request.ready) {
+        console.log("Readiness of " + sender.tab.id + " is 'ready'");
+        readiness[sender.tab.id] = "ready";
+      }
+      else if (request.toggle) {
+        toggle();
+      } else if (request.showdump) {
+        toggleOff();
+        showDump();
+      } else {
+        snippet = {
+          content: request.content,
+          url: request.url
+        };
+        snippage.snippets[snippage.snippets.length] = snippet;
+        localStorage.setItem('snippage', $.toJSON(snippage));
+        updateBadgeText();
+      }
+      sendResponse({});
+    }
+  );  
+}
+
+
+/*
+  Toggles snippy activation status on the current tab, and sends a message
+  to the tab itself.
+*/
 function toggle() {
   chrome.tabs.getSelected(null, function(tab) {
     if (activation[tab.id]) {
@@ -32,6 +156,10 @@ function toggle() {
   });
 }
 
+
+/*
+  Toggles snippy off, if active on the current tab, and notifies the tab.
+*/
 function toggleOff() {
   chrome.tabs.getSelected(null, function(tab) {
     if (activation[tab.id]) {
@@ -41,6 +169,12 @@ function toggleOff() {
   });
 }
 
+
+/*
+  Activates the 'dump' tab, showing the list of collected snippets.
+  If the 'dump' tab already exists, we just shift the focus to it,
+  otherwise we open a new one.
+*/
 function showDump() {
   if (dump_tab_id) {
     chrome.tabs.update(dump_tab_id, { selected: true });
@@ -52,14 +186,28 @@ function showDump() {
   }
 }
 
+
+/*
+  Returns the current snippage.
+*/
 function getSnipPage() {
   return snippage;
 }
 
+
+/*
+  Persists the current snippage to localStorage.
+*/
 function updateLocalStorage() {
   localStorage.setItem('snippage', $.toJSON(snippage));
 }
 
+
+/*
+  Clears the current snippage and reverts it to a blank one.
+  Updates the badge text, removes the snippage from localStorage and
+  closes the 'dump' tab (if open).
+*/
 function clearCopy() {
   snippage.title = '';
   snippage.snippets = [];
@@ -70,14 +218,26 @@ function clearCopy() {
   }
 }
 
+
+/*
+  Shows the about page.
+*/
 function about() {
   chrome.tabs.create({"url": "about.html"});
 }
 
+
+/*
+  Shows the bugs page.
+*/
 function bug() {
   chrome.tabs.create({"url": "bug.html"});
 }
 
+
+/*
+  Updates the badge text to reflect the number of current snippets.
+*/
 function updateBadgeText() {
   if (snippage.snippets.length == 0) {
     chrome.browserAction.setBadgeText({'text': ''});
@@ -86,6 +246,11 @@ function updateBadgeText() {
   }
 }
 
+
+/*
+  Returns the readiness status of the currently selected tab. It can be
+  either 'unknown', 'ready' or 'loading'.
+*/
 function CurrentTabReadiness() {
   if (!readiness[cur_tab_id]) {
     return "unknown";
@@ -94,59 +259,11 @@ function CurrentTabReadiness() {
   }
 }
 
+
+/*
+  Returns the url of the currently selected tab.
+*/
 function CurrentTabUrl() {
   return cur_tab_url;
 }
 
-chrome.tabs.onRemoved.addListener(function(tab_id) {
-  if (dump_tab_id == tab_id) {
-    dump_tab_id = null;
-  }
-});
-
-chrome.tabs.onSelectionChanged.addListener(function(tab_id) {
-  cur_tab_id = tab_id;
-  if (dump_tab_id == tab_id) {
-    chrome.tabs.sendRequest(tab_id, {'reload': true}, function(response){});
-  }
-  chrome.tabs.get(tab_id, function(tab) {
-    cur_tab_url = tab.url;
-  });
-});
-
-chrome.tabs.onUpdated.addListener(function(tab_id, change_info) {
-  if (change_info.status == "loading") {
-    console.log("Readiness of " + tab_id + " is 'loading'");
-    readiness[tab_id] = "loading";
-  } else if (change_info.status == "complete") {
-    console.log("Readiness of " + tab_id + " is 'complete'");
-    readiness[tab_id] = "ready";
-  }
-  chrome.tabs.get(tab_id, function(tab) {
-    cur_tab_url = tab.url;
-  });
-});
-
-chrome.extension.onRequest.addListener(
-  function(request, sender, sendResponse) {
-    if (request.ready) {
-      console.log("Readiness of " + sender.tab.id + " is 'ready'");
-      readiness[sender.tab.id] = "ready";
-    }
-    else if (request.toggle) {
-      toggle();
-    } else if (request.showdump) {
-      toggleOff();
-      showDump();
-    } else {
-      snippet = {
-        content: request.content,
-        url: request.url
-      };
-      snippage.snippets[snippage.snippets.length] = snippet;
-      localStorage.setItem('snippage', $.toJSON(snippage));
-      updateBadgeText();
-    }
-    sendResponse({});
-  }
-);
--- ext/js/dump.js
+++ ext/js/dump.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2009 Riccardo Govoni. All rights reserved.
+// Use of this source code is governed by the MIT License and Creative Commons
+// Attribution License 3.0. Both of them can be found in the LICENSE file.
+
 var snippage;
 var defaultSnippageTitle = 'Click here to add a title to your snippet';
 
