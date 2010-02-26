740b591e764c32c0e97a21b56ec57ccabdbdb6d9
Riccardo Govoni <battlehorse@gmail.com>
2010-02-26 15:45:11 +0000

    Making the options page update in realtime if Snippy configuration changes elsewhere.

--- ext/js/options.js
+++ ext/js/options.js
@@ -3,17 +3,27 @@
 // Attribution License 3.0. Both of them can be found in the LICENSE file.
 
 $(document).ready(function() {
-  // Current oauth access status.
+  $('#snippy-docs-oauth-token').click(function() {
+    oauth = chrome.extension.getBackgroundPage().getOAuth();
+    oauth.clearTokens();
+    $(this).text('Snippy cannot access your Google Docs').
+      attr('disabled', 'disabled');
+  });
+
+
+  loop();
+});
+
+function loop() {
+  // Keep checking for changes in the extension that requires updates
+  // to the extension options.
   oauth = chrome.extension.getBackgroundPage().getOAuth();
   if (oauth.hasToken()) {
-    $('#snippy-docs-oauth-token').text('Revoke permission').click(function() {
-      debugger;
-      oauth.clearTokens();
-      $(this).text('Snippy cannot access your Google Docs').
-        attr('disabled', 'disabled');
-    });
+    $('#snippy-docs-oauth-token').removeAttr('disabled').
+      text('Revoke permission');
   } else {
     $('#snippy-docs-oauth-token').text('Snippy cannot access your Google Docs').
       attr('disabled', 'disabled');
   }
-});
+  window.setTimeout(loop, 500);
+};
